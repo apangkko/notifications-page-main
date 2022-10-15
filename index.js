b56afc3423787ab7f2ebb8f9cafbc9b64292ac04
APANG <apangkko@gmail.com>
2022-10-15 18:30:17 +0700

    v2

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const unread = document.querySelectorAll(".unread");
+const notification = document.querySelectorAll(".notification");
 const unreadCount = document.querySelector(".noti__unread_count");
 const markUnread = document.querySelector(".markall__asread");
 
 unreadCount.innerHTML = unread.length;
 
 markUnread.addEventListener("click", () => {
-    unread.forEach(element => {
-        element.classList.remove("unread");
+    unread.forEach(noti => {
+        noti.classList.remove("unread");
     });
-
-    unreadCount.innerHTML = 0;
+    const unreadCheck = document.querySelectorAll(".unread");
+    unreadCount.innerHTML = unreadCheck.length;
 })
+
+notification.forEach(noti => {
+    noti.addEventListener("click", () => {
+        noti.classList.remove("unread");
+        const unreadCheck = document.querySelectorAll(".unread");
+        unreadCount.innerHTML = unreadCheck.length;
+    })
+});
